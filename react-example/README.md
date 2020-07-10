8d0c4c0a45fff21a4466a0ae0ce09d749851140a
Nessie <694883+n3ssi3@users.noreply.github.com>
2020-07-10 13:58:59 +0200

    Simple example with reactjs (#6)
    Co-authored-by: Patrick Stadler <patrick.stadler@datatrans.ch>

---
+++ react-example/README.md
@@ -0,0 +1,9 @@
+# Datatrans Payment Button with React
+
+Render Apple Pay and Google Pay buttons for payments via Datatrans. Buttons are only rendered if the client supports any of the mentioned payment methods.
+
+## Development
+```
+npm install
+npm start
+```
---
+++ react-example/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import PaymentButton from './paymentButton'
+
+const App = <>
+  <h1 style={{ fontFamily: '"Volte-Medium", "Helvetica Neue", Helvetica, Arial', color: '#213d62' }}>Datatrans Payment Button</h1>
+  <h2 style={{ fontFamily: '"Volte-Medium", "Helvetica Neue", Helvetica, Arial', color: '#213d62' }}>with React</h2>
+
+  <PaymentButton
+    merchantId='1000011011'
+    merchantName='Test'
+    options={{
+      allowedCardNetworks: ['MASTERCARD', 'VISA']
+    }}
+    payment={{
+      // W3C spec
+      details: {
+        total: {
+          label: "My Merchant",
+          amount: { value: "4", currency: "USD" },
+        },
+        displayItems: [
+          {
+            label: "Tax",
+            amount: { value: "2.50", currency: "USD" },
+          }
+        ]
+      },
+      // W3C spec
+      options: {
+        requestPayerEmail: false,
+        requestPayerName: false,
+        requestPayerPhone: false,
+      },
+      // Datatrans specific
+      transaction: {
+        countryCode: "CH",
+        refno: "3e23dasdasd1123"
+      }
+    }} />
+</>
+
+ReactDOM.render(
+  App,
+  document.getElementById('app')
+);
