d9fa2f0232ddb0aa12a96d72f4d2140750eb3bc9
Patrick Stadler <patrick.stadler@datatrans.ch>
2025-09-25 14:24:25 +0200

    update dependencies

--- react-example/PaymentButtonWithHook.js
+++ react-example/PaymentButtonWithHook.js
@@ -6,8 +6,8 @@ const getUrl = (production) =>
     : 'https://pay.sandbox.datatrans.com/upp/payment/js/payment-button-1.0.0.js'
 
 const DEFAULT_OPTIONS = {
-  useGooglePay: false,
-  useApplePay: false,
+  useGooglePay: true,
+  useApplePay: true,
   auto: true,
   autoSettle: false,
   tokenOnly: false,
